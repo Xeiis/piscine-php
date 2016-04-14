a07ce547be7018cb4f97d97a13d40411f2dde8c9
xeiis <seis02@hotmail.fr>
2016-04-14 15:59:46 +0200

    j09 ex03 fini

---
+++ j09/ex02/todo.js
@@ -0,0 +1,50 @@
+var x = getCookie('all');
+var res = x.replace(/-/g, "<div id=\"todo\" onclick=\"remove();suppr()\" class=\"all\">");
+var res = res.replace(/\//g, "</div>");
+document.getElementById('ft_list').innerHTML = res;
+
+function test()
+{
+	var all = document.getElementsByClassName('all');
+	var str = "";
+	var rechCount = all.length;
+	for(i = 0; i < rechCount; i++)	{
+		str += "-" +all[i].innerHTML + "/";
+	}
+	return str;
+}
+function suppr()
+{
+	setCookie('all', test(), 1);
+}
+//document.getElementById('ft_list').innerHTML = x;
+function AddTodo() {
+	var person = prompt("Please enter a To do", "");
+	var who = document.createElement('div');
+	who.setAttribute("id", "todo");
+	who.setAttribute("onclick", "remove();suppr()");
+	who.setAttribute("class", "all");
+	var where = document.getElementById('ft_list');
+	who.innerHTML = person;
+	if (person) {
+		where.insertBefore(who, where.firstChild);
+	}
+	setCookie('all', test(), 1);
+}
+
+function setCookie(cname, cvalue, exdays) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	var expires = "expires="+d.toUTCString();
+	document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+function getCookie(cname) {
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1);
+		if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+	}
+	return "";
+}
